0fc5c2ceaeb784a7b0e1ed70171acc9a8981d23b
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-12-03 19:26:30 -0600

    Create events.php

---
+++ events.php
@@ -0,0 +1,34 @@
+<?php
+$pageTitle = 'Events';
+include 'view-header.php';
+include 'dbconfig.php';
+
+$query = "SELECT * FROM Events";
+$stmt = $conn->query($query);
+$events = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<h1>Events</h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Event Name</th>
+      <th>Event Date</th>
+      <th>Location</th>
+      <th>Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($events as $event): ?>
+      <tr>
+        <td><?php echo htmlspecialchars($event['EventName']); ?></td>
+        <td><?php echo htmlspecialchars($event['EventDate']); ?></td>
+        <td><?php echo htmlspecialchars($event['Location']); ?></td>
+        <td>
+          <a href="sessions.php?event_id=<?php echo $event['EventID']; ?>">View Sessions</a> |
+          <a href="participants.php?event_id=<?php echo $event['EventID']; ?>">View Participants</a>
+        </td>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
