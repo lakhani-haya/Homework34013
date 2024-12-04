f598203aa35cbd5bedf59ed017c2b36f233a9c34
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-12-03 19:33:11 -0600

    Update participants.php

--- participants.php
+++ participants.php
@@ -1,43 +1,40 @@
+
+php
+Copy code
 <?php
 include 'dbconfig.php';
-$eventID = $_GET['event_id'];
-$query = "SELECT * FROM Participants WHERE EventID = ?";
-$stmt = $conn->prepare($query);
-$stmt->execute([$eventID]);
+include 'view-header.php';
+
+$eventID = isset($_GET['event_id']) ? $_GET['event_id'] : null;
+
+if ($eventID) {
+    $query = "SELECT * FROM Participants WHERE EventID = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->execute([$eventID]);
+} else {
+    $query = "SELECT * FROM Participants";
+    $stmt = $conn->query($query);
+}
+
 $participants = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Participants</title>
-</head>
-<body>
-    <nav>
-        <a href="index.php">Events</a>
-        <a href="participants.php">Participants</a>
-        <a href="sessions.php">Sessions</a>
-        <a href="speakers.php">Speakers</a>
-    </nav>
-
-    <h1>Participants</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>Participant Name</th>
-                <th>Email</th>
-                <th>View Tickets</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($participants as $participant): ?>
-            <tr>
-                <td><?php echo htmlspecialchars($participant['ParticipantName']); ?></td>
-                <td><?php echo htmlspecialchars($participant['Email']); ?></td>
-                <td><a href="tickets.php?participant_id=<?php echo $participant['ParticipantID']; ?>">View Tickets</a></td>
-            </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-</body>
-</html>
+<h1>Participants</h1>
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th>Participant Name</th>
+            <th>Email</th>
+            <th>View Tickets</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($participants as $participant): ?>
+        <tr>
+            <td><?php echo htmlspecialchars($participant['ParticipantName']); ?></td>
+            <td><?php echo htmlspecialchars($participant['Email']); ?></td>
+            <td><a href="tickets.php?participant_id=<?php echo $participant['ParticipantID']; ?>">View Tickets</a></td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
