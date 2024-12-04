55ea568250f9ba50e56d285e28edcbc7062444a9
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-12-03 18:24:49 -0600

    Update index.php

--- index.php
+++ index.php
@@ -10,17 +10,47 @@ $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <html>
 <head>
     <title>Events</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+        th, td {
+            padding: 8px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
 </head>
 <body>
     <h1>Events</h1>
-    <ul>
-        <?php foreach ($events as $event): ?>
-        <li>
-            <?php echo htmlspecialchars($event['EventName']); ?> - <?php echo $event['EventDate']; ?> (<?php echo $event['Location']; ?>)
-            <a href="participants.php?event_id=<?php echo $event['EventID']; ?>">View Participants</a>
-            <a href="sessions.php?event_id=<?php echo $event['EventID']; ?>">View Sessions</a>
-        </li>
-        <?php endforeach; ?>
-    </ul>
+    <table>
+        <thead>
+            <tr>
+                <th>Event Name</th>
+                <th>Event Date</th>
+                <th>Location</th>
+                <th>View Participants</th>
+                <th>View Sessions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($events as $event): ?>
+            <tr>
+                <td><?php echo htmlspecialchars($event['EventName']); ?></td>
+                <td><?php echo $event['EventDate']; ?></td>
+                <td><?php echo $event['Location']; ?></td>
+                <td><a href="participants.php?event_id=<?php echo $event['EventID']; ?>">View Participants</a></td>
+                <td><a href="sessions.php?event_id=<?php echo $event['EventID']; ?>">View Sessions</a></td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 </body>
 </html>
+
