d4dfc80c838b9a91b1478cb2330b906e33ee30a6
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-12-05 08:20:39 -0600

    Update participants.php

--- participants.php
+++ participants.php
@@ -9,7 +9,58 @@ $stmt->execute($eventID ? [$eventID] : []);
 $participants = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
-<h1>Participants</h1>
+<div class ="row">
+    <div class="col"
+  <h1>Participants</h1>
+    </div>
+    <div class="col-auto">
+        
+    </div>
+  </div>
+
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newParticipantModal">
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+</svg>
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="newParticipantModal" tabindex="-1" aria-labelledby="newParticipantModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="newParticipantModalLabel">New Participant</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+       <form>
+        <form method="post" action="">
+  <div class="mb-3">
+    <label for="ParticipantName" class="form-label">Participant Name</label>
+    <input type="text" class="form-control" id="ParticipantName" name="ParticipantName">    
+  </div>
+ <div class="mb-3">
+    <label for="Email" class="form-label">E-mail</label>
+    <input type="Email" class="form-control" id="Email" name="Email">    
+  </div>
+ <div class="mb-3">
+    <label for="EventID" class="form-label">EventID</label>
+    <input type="text" class="form-control" id="EventID" name="EventID">    
+  </div>
+ 
+  <button type="submit" class="btn btn-primary">Save</button>
+</form>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
 <table class="table table-striped">
     <thead>
         <tr>
